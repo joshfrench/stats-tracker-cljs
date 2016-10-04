2aee30d0f3a03ea4da0a7917487c1c2c049762ac
Josh French <joshfrench@gmail.com>
2016-10-03 20:40:16 -0700

    Working formulas

--- src/thirteenth_age/macros.clj
+++ src/thirteenth_age/macros.clj
@@ -5,4 +5,4 @@
 
 (defmacro klass [name & fns]
   (let [stats (map formula->fn fns)]
-    (do `(defrecord ~name [~(symbol 'hp)] IKlass ~@['(ac [_] (+ hp 1))]))))
+    (do `(defrecord ~name [~(symbol 'hp)] IKlass ~@(map formula->fn fns)))))
