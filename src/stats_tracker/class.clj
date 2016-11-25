3174807fa70133d26e8afa5dadace28bcdcd82af
Josh French <joshfrench@gmail.com>
2016-11-25 14:16:34 -0800

    Working stat macros

--- src/stats_tracker/class.clj
+++ src/stats_tracker/class.clj
@@ -1,14 +1,8 @@
 (ns stats-tracker.class)
 
-(defmacro base-hp [hp]
-  `{:hp (* (+ ~hp ~'(modifier con)) ~'(hp-multiplier level))})
-
-(defmacro base-ac [ac]
-  `{:ac 12})
-
 (defmacro class [name & stats]
   `(def ~name
      (with-meta
        (fn [{:keys ~'[level str con dex wis int cha]}]
-         (merge ~@(partition 2 stats)))
+         (merge-with merge ~@(partition 2 stats)))
        {:name (str '~name)})))
